4c247538fc8f9a89165d3bbc7acce7cbb847f151
glenorock <glen19.og@gmail.com>
2022-03-10 15:33:54 +0100

    header v1

--- components/header.js
+++ components/header.js
@@ -1,16 +1,19 @@
 import Image from 'next/image'
+import styles from './styles/header.module.css'
 
 export default function Header(){
     return(
-        <div>
-            <span>Agence Nationale des Technologies de L'information et de la Communication</span>
-            <Image
-                src="/images/logo.jpg"
-                height={144}
-                width={144}
-                alt="logo"
-            />
-            <span>National Agency for Information and Communication Technologies</span>
+        <div className={styles.header}>
+            <div className={styles.name}>Agence Nationale des Technologies de L'information et de la Communication</div>
+            <div className={styles.header_logo}>
+                <Image
+                    src="/images/logo.jpg"
+                    alt="logo"
+                    width={90}
+                    height={90}
+                />
+            </div>
+            <div className={styles.name}>National Agency for Information and Communication Technologies</div>
         </div>
     )
 }
